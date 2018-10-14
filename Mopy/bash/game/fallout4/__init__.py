19bd9f1ffb8d9680e97e6ad422ef318770d8e43d
Sharlikran <danielhmpdx16@gmail.com>
2018-10-13 18:57:11 -0700

    Update Fallout 4 records.py
    Imports the same as Skyrim SE to facilitate better importing
    Updated Leveled List definitions as they are different from Skyrim
    
    EDITED:
     - Corrected two typos: skyrim SE -> Fallout 4

--- Mopy/bash/game/fallout4/__init__.py
+++ Mopy/bash/game/fallout4/__init__.py
@@ -121,8 +121,12 @@ class Fallout4GameInfo(GameInfo):
 
     patchers = (u'ListsMerger',)
 
+    # ---------------------------------------------------------------------
+    # --Imported - MreGlob is special import, not in records.py
+    # ---------------------------------------------------------------------
     @classmethod
     def init(cls):
+        # First import from fallout4.records file, so MelModel is set correctly
         from .records import MreHeader, MreLvli, MreLvln
         # ---------------------------------------------------------------------
         # These Are normally not mergable but added to brec.MreRecord.type_class
@@ -137,7 +141,11 @@ class Fallout4GameInfo(GameInfo):
         #
         #       MreAchr, MreDial, MreLctn, MreInfo, MreFact, MrePerk,
         # ---------------------------------------------------------------------
-
+        cls.mergeClasses = (
+            # -- Imported from Skyrim/SkyrimSE
+            # Added to records.py
+            MreLvli, MreLvln
+        )
         # Setting RecordHeader class variables --------------------------------
         brec.RecordHeader.topTypes = [
             'GMST', 'KYWD', 'LCRT', 'AACT', 'TRNS', 'CMPO', 'TXST', 'GLOB',
@@ -163,9 +171,10 @@ class Fallout4GameInfo(GameInfo):
                            'PMIS','DLBR','DIAL','INFO','SCEN'})
         brec.RecordHeader.plugin_form_version = 131
         brec.MreRecord.type_class = dict((x.classType,x) for x in (
-            MreLvli, MreLvln,
-            ####### for debug
-            MreHeader,
+            #--Always present
+            MreHeader, MreLvli, MreLvln,
+            # Imported from Skyrim or SkyrimSE
+            # Added to records.py
             ))
         brec.MreRecord.simpleTypes = (
             set(brec.MreRecord.type_class) - {'TES4',})
